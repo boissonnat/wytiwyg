66ab49710bc7532075400c138ef1788d2e6fa350
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2015-03-24 21:22:14 +0100

    Add animated icon on save document button during the Ajax call

--- app/assets/javascript/mouApp.js
+++ app/assets/javascript/mouApp.js
@@ -64,3 +64,21 @@ mouApp.directive('documentInfos', function () {
         controller: 'DocumentsCtrl'
     }
 });
+
+mouApp.directive('waitButton',   ['$http' ,function ($http) {
+    return {
+        link: function (scope, elm) {
+            scope.isLoading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+            scope.$watch(scope.isLoading, function (v) {
+                if(v){
+                    elm.show();
+                }else{
+                    elm.hide();
+                }
+            });
+        }
+    };
+
+}]);
